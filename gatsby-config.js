1bb934f43c501678c602383961c36b83ad98131b
Ondřej Chrastina <chrastina.ondra@gmail.com>
2022-03-16 12:44:07 +0100

    prepare sample Kontent configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: '@kentico/gatsby-source-kontent',
+      options: {
+        projectId: '975bf280-fd91-488c-994c-2f04416e5ee3', // Fill in your Project ID
+        languageCodenames: [
+          'en-US', // Languages in your project (Project settings -> Localization),
+          'es-ES', // Languages in your project (Project settings -> Localization),
+        ],
+      },
+    },
+  ],
 }
